f3fba897f59f0a455a62993e8e48f1b3455642fa
lizzyjaynes <144627533+lizzyjaynes@users.noreply.github.com>
2025-11-03 01:05:16 -0500

    level building from files!

--- A/Level.java
+++ A/Level.java
@@ -1,5 +1,9 @@
 import java.awt.Graphics;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
 import java.util.List;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 /**
@@ -10,22 +14,20 @@ import java.util.ArrayList;
  */
 public class Level {
 	public Player player;
-	public List<Platform> platforms;
-	public List<Enemy> enemies;
-	public List<Collectibles> blocks;
+	public List<Platform> platforms = new ArrayList<>();
+	public List<Enemy> enemies = new ArrayList<>();
+	public List<Collectibles> blocks = new ArrayList<>();
 	private Hud hud;
+	String fileName;
 
-	public Level() {
-		player = new Player(100, 100);
+	public Level(String fileName) {
+		this.fileName = fileName;
+		loadFromFile();
 		hud = new Hud(player);
-
-		platforms = new ArrayList<>();
-		enemies = new ArrayList<>();
-		blocks = new ArrayList<>();
-
-		platforms.add(new Platform(0, 400, 600, 30));
-		enemies.add(new Enemy(300, 330));
-		blocks.add(new Collectibles(250, 360, 10));
+		/*
+		 * player = new Player(100, 100); platforms.add(new Platform(0, 400, 600, 30));
+		 * enemies.add(new Enemy(300, 330)); blocks.add(new Collectibles(250, 360, 10));
+		 */
 	}
 
 	public void update() {
@@ -53,5 +55,47 @@ public class Level {
 		player.draw(g);
 
 		hud.draw(g);
+	}
+
+	public void loadFromFile() {
+		Scanner scanner = null;
+		File f = null;
+		try {
+			f = new File(fileName);
+			scanner = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+		// frame size = 600, 450
+		int lineNum = 0;
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			lineNum++;
+			for (int i = 0; i < line.length(); i++) {
+				char c = line.charAt(i);
+				int x = i * 30;
+				int y = lineNum * 30;
+
+				switch (c) {
+				case '_':
+					platforms.add(new Platform(x, y, 30, 30));
+					break;
+				case 'e':
+					enemies.add(new Enemy(x-(50/2), y-(70/2)));
+					break;
+				case 'b':
+					blocks.add(new Collectibles(x, y, 10));
+					break;
+				case 'p':
+					player = new Player(x, y);
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		scanner.close();
+
 	}
 }
