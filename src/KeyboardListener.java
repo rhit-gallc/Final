406822f7e0bb31b49fd5a6d4cb94d080f0826662
lizzyjaynes <144627533+rhit-jaynesea@users.noreply.github.com>
2025-11-04 14:22:22 -0500

    start of edits

---
+++ src/KeyboardListener.java
@@ -0,0 +1,25 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardListener implements KeyListener {
+	Player player;
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/MainApp.java
@@ -0,0 +1,16 @@
+
+public class MainApp {
+	
+	final int F_HEIGHT = 800;
+	final int F_WIDTH = 600;
+	Display frame;
+	
+	public MainApp() {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
